952c861059ce04ba6685889af00c8ae0e3756abc
kastashenkova <astashenkova.katya@gmail.com>
2025-12-04 01:28:16 +0200

    created ball lottery

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+
+public class Ball {
+    private final String color;
+    private final int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    enum Color {
+        YELLOW,
+        RED,
+        BLUE,
+        GREEN,
+        BLACK,
+        WHITE,
+        ORANGE
+    }
+
+    @Override
+    public String toString() {
+        return "Ball [color=" + color + ", number=" + number + "]";
+    }
+}
